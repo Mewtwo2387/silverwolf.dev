2534b4446f82250e5ddac8d07b4e302bb17bbc8d
hhw23 <hhw23@ic.ac.uk>
2025-02-10 00:36:46 +0000

    fix: fixed all links (those will be served by nginx)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
     {
       title: "Imperial CS Grade Tracker",
       description: "Grade Tracker for Y1 and Y2 Computing. The only useful thing in this list.",
-      link: "/Tracker",
+      link: "/tracker",
       hoverText: "Click to enter!",
       status: "Completed and maintained",
       statusType: "online"
@@ -27,7 +27,7 @@ export default function Home() {
     {
       title: "Avaritilax 4",
       description: "4th game of the Idle Snorlax series. An idle incremental game about feeding a Snorlax, with additional minigames such as mining, crafting, gacha, missions, and more!",
-      link: "/Avaritilax",
+      link: "/avaritilax",
       hoverText: "Click to play!",
       status: "Alpha 1.1.3. Development halted, beta soon™",
       statusType: "online"
@@ -35,7 +35,7 @@ export default function Home() {
     {
       title: "Silverwolf Bot",
       description: "A multipurpose discord bot made by Ei and Xei with... a lot of gambling. And 90 other useless commands.",
-      link: "/Silverwolf",
+      link: "/silverwolf",
       hoverText: "Click for more info!",
       status: "Active Development",
       statusType: "online"
@@ -52,7 +52,7 @@ export default function Home() {
     {
       title: "QueQat",
       description: "An AI made with Deep Q-Learning, used to reduce queueing time in multi-queue systems where different people have different service times, such as supermarkets. QueQat can assign customers to queues based on their service time, and can reduce the total waiting time by approximately 20%.",
-      link: "/QueQat",
+      link: "/queqat",
       hoverText: "Click for more info!",
       status: "Completed, but server is offline.",
       statusType: "offline"
@@ -60,7 +60,7 @@ export default function Home() {
     {
       title: "DseGPT",
       description: "A website to assist English teachers in marking essays, using ChatGPT. It will give a grade and help teachers write comments based on custom marking schemes and instructions, while also providing grammar corrections and sample rewrites.",
-      link: "/DseGPT",
+      link: "/dsegpt",
       hoverText: "Click for nothing.",
       status: "It's from my internship 2 years ago, but eh, it's probably dead.",
       statusType: "discontinued"
@@ -68,7 +68,7 @@ export default function Home() {
     {
       title: "Project Keqei",
       description: "An online multiplayer turn-based RPG 6v6 gacha game, featuring members from TheGenshinPlace (and OC made by the members) as the characters. The storyline focuses on Ei breaking the fourth wall and excaping the world, while Keq and the egirls trying to fix the glitches and diasters caused by Ei.",
-      link: "/ProjectKeqei",
+      link: "/keqei",
       hoverText: "Click for the dead game.",
       status: "Discontinued",
       statusType: "discontinued"
@@ -79,7 +79,7 @@ export default function Home() {
     {
       title: "Keqboard",
       description: `A completely useless program that "translates" stuff into weirder stuff (,,,,,,ffffffuwut9s[ wedwieuwu[ otni[ ffffffuwut9s[ "9setw9snwt"[ tht[ mwgowp[ 9s9sewe9suwu[ eiwetewpmoc[  ,,,!!!eifmoo iah)`,
-      link: "/Keqboard",
+      link: "/keqboard",
       hoverText: "Click to enter!",
       status: "Adding stuff whenever I find another weird keyboard",
       statusType: "online"
@@ -87,7 +87,7 @@ export default function Home() {
     {
       title: "Binary Collatz Sequence",
       description: "The Collatz Sequence forms some cool pattern in binary for big numbers. For those that want to go through the unavoidable process of attempting to solve the conjecture and failing.",
-      link: "/Collatz",
+      link: "/collatz",
       hoverText: "Click to enter!",
       status: "Completed",
       statusType: "online"
@@ -95,7 +95,7 @@ export default function Home() {
     {
       title: "Register Machines",
       description: "Simulate register machines for Imperial College's COMP50003 module",
-      link: "/RM",
+      link: "/rm",
       hoverText: "Click to enter!",
       status: "Completed",
       statusType: "online"
@@ -107,16 +107,15 @@ export default function Home() {
     {
       title: "Ei Bot",
       description: "A bot made for TGP's Basement.",
-      link: "/Ei",
+      link: "https://github.com/Mewtwo2387/ei2",
       hoverText: "Click to check the code i guess",
       status: "Discontinued, replaced by Silverwolf Bot",
       statusType: "discontinued"
     },
     {
       title: "Idle Snorlax 3",
-
       description: "The third game of the Idle Snorlax series. Made when I first learned HTML.",
-      link: "/SC3",
+      link: "/sc3",
       hoverText: "Click to play! But why would you? There's Avaritilax 4 up there.",
       status: "Discontinued, replaced by Avaritilax 4",
       statusType: "discontinued"
@@ -124,7 +123,7 @@ export default function Home() {
     {
       title: "Idle Snorlax 2",
       description: "The second game of the Idle Snorlax series. Made in App Inventor 2, 0.1 Amber Eras ago.",
-      link: "/SC2",
+      link: "/sc2",
       hoverText: "Click to download the .apk for android! (No virus fr, trust me)",
       status: "Discontinued, replaced by Idle Snorlax 3",
       statusType: "discontinued"
@@ -132,7 +131,7 @@ export default function Home() {
     {
       title: "Idle Snorlax 1",
       description: "The first game of the Idle Snorlax series. Made in App Inventor 2, >0.1 Amber Eras ago.",
-      link: "/SC1",
+      link: "/sc1",
       hoverText: "Click to download the .apk for android! (No virus fr, trust me)",
       status: "Discontinued, replaced by Idle Snorlax 2",
       statusType: "discontinued"
@@ -143,7 +142,7 @@ export default function Home() {
     {
       title: "Idle Bubble",
       description: "Made for the 2025 Global Game Jam with theme 'Bubble'. Idle Bubble is an idle incremental game where you earn credits by popping rising bubbles, which can be used to buy various upgrades increasing the value and frequency of bubbles, and unlock different bubble variants.",
-      link: "/Bubble",
+      link: "/ggj25",
       hoverText: "Click to play!",
       status: "Completed",
       statusType: "online"
